51b8caa24711fa0788d17dd280d0034295d7c4dd
God Chama <99276653+AleMar21430@users.noreply.github.com>
2023-09-17 14:36:18 -0600

    Commit

--- Proyecto 1.py
+++ Proyecto 1.py
@@ -19,16 +19,18 @@ infix_expr = "(a|b)?+ab*abb"
 #expr = input("Expresión regular r:  ")
 
 postfix_expr = infix_to_postfix(infix_expr)
-print(f"Infix:   {infix_expr}")
-print(f"Postfix: {postfix_expr}")
 
 syntax_tree = build_syntax_tree(postfix_expr)
 nfa_start, nfa_end = build_nfa(syntax_tree)
-
 dfa = nfa_to_dfa(nfa_start, nfa_end)
 
+print("\n-----------------------------Regex--------------------------------")
+print(f"Infix:   {infix_expr}")
+print(f"Postfix: {postfix_expr}")
+print("---------------------------Simulation-----------------------------")
 print(f"La Cadena w: {expr} pertenece a {infix_expr}  ?   w ∈ L(r) NFA: {simulate_nfa((nfa_start, nfa_end),expr)}")
 print(f"La Cadena w: {expr} pertenece a {infix_expr}  ?   w ∈ L(r) DFA: {simulate_dfa(dfa, expr)}")
+print("------------------------------------------------------------------\n")
 
 graph = visualize_stt(syntax_tree)
 graph.view()
